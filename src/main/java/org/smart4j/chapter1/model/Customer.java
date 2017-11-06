75e4af55575b4110cd5dd51e13c0df96a2669aa1
owenyy <ouyangwenyu@mail.dlut.edu.cn>
2017-11-06 14:48:51 +0800

    第二章的一部分

--- src/main/java/org/smart4j/chapter1/model/Customer.java
+++ src/main/java/org/smart4j/chapter1/model/Customer.java
@@ -6,4 +6,44 @@ public class Customer {
     private String telephone;
     private String email;
     private String remark;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContract() {
+        return contract;
+    }
+
+    public void setContract(String contract) {
+        this.contract = contract;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
 }
